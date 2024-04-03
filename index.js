3cdee50835e044dac20faa8da59b69f78027db98
brianchege2k <chegebrian84@gmail.com>
2024-04-03 12:01:52 +0300

    objects modification lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution in this file!
+const employee = {
+    employeeName: 'Brian Chege',
+    streetAddress: '90210 Valley Lane'
+
+};
+
+function updateEmployeeWithKeyAndValue(obj, name, value){
+
+    const newObj = {...obj};
+
+    newObj[name] = value;
+
+
+
+    return newObj;
+    
+
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee, 'emoloyeeName', 'Sam' )
+
+console.log(newEmployee)
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj [key] = value;
+    return obj;
+}
+
+const anotherEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,'employeeName','Sam')
+
+
+console.log(anotherEmployee)
+
+
+function deleteFromEmployeeByKey (obj,key){
+
+    const newObj = delete {...obj[key]}
+
+    return newObj;
+
+}
+
+
+const anotherNewEmployee = deleteFromEmployeeByKey (employee,'streetAddress')
+
+console.log(anotherNewEmployee)
+
